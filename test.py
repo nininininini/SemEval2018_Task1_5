7d5c0398f7a8780be055baa486a673de5c2d32a1
Oliver <oliver.deck@gmail.com>
2017-12-30 14:11:19 +0100

    1

--- test.py
+++ test.py
@@ -10,17 +10,17 @@ nltk tweet tokenizer
 """
 
 import os
-# import keras
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import KFold
-from timeit import timeit
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import TweetTokenizer
 from collections import Counter
+
+import emoji
 
 from keras.preprocessing import sequence
 from keras.models import Sequential
 from keras.layers import Dense, Embedding, LSTM, Bidirectional
+from keras.layers import Conv1D, GlobalMaxPooling1D
 
 
 def create_dictionary(texts, vocab_size):
@@ -47,20 +47,30 @@ def to_ids(words, dictionary):
         ids.append(dictionary.get(word, dictionary[UNKNOWN_TOKEN]))
     return ids
 
+def extract_emojis(str):
+  return [c for c in str if c in emoji.UNICODE_EMOJI]
 
 def read_data(train_file, dev_file):
+    tokenizer = TweetTokenizer()
     trainDF = pd.read_csv(train_file, sep='\t')
     trainDF = trainDF.reindex(np.random.permutation(trainDF.index))
-    trainDF.insert(1, 'tweet_tokenized', (trainDF['Tweet'].apply(word_tokenize)))
+    trainDF.insert(1, 'tweet_tokenized', (trainDF['Tweet'].apply(lambda x: tokenizer.tokenize(x))))
+    trainDF.insert(1, 'emojis', (trainDF['tweet_tokenized'].apply(lambda x: extract_emojis(x))))
 
     devDF = pd.read_csv(dev_file, sep='\t')
     devDF = devDF.reindex(np.random.permutation(devDF.index))
-    devDF.insert(1, 'tweet_tokenized', (devDF['Tweet'].apply(word_tokenize)))
+    devDF.insert(1, 'tweet_tokenized', (devDF['Tweet'].apply(lambda x: tokenizer.tokenize(x))))
+    devDF.insert(1, 'emojis', (devDF['tweet_tokenized'].apply(lambda x: extract_emojis(x))))
 
-    word2id = create_dictionary(trainDF["tweet_tokenized"], VOCAB_SIZE)
+    word2id = create_dictionary(trainDF['tweet_tokenized'], VOCAB_SIZE)
+
 
     trainDF.insert(1, 'tweet_ids', (trainDF['Tweet'].apply(lambda x: to_ids(x, dictionary=word2id))))
     devDF.insert(1, 'tweet_ids', (devDF['Tweet'].apply(lambda x: to_ids(x, dictionary=word2id))))
+
+    emoji2id = create_dictionary(trainDF['emojis'], 500)
+    trainDF.insert(1, 'emoji_ids', (trainDF['emojis'].apply(lambda x: to_ids(x, dictionary=emoji2id))))
+    devDF.insert(1, 'emoji_ids', (devDF['emojis'].apply(lambda x: to_ids(x, dictionary=emoji2id))))
 
     return trainDF, devDF
 
@@ -89,13 +99,33 @@ class emotionNN:
             verbose=2
         )
 
-        return lstm_model.evaluate(self.x_dev, self.y_dev)
+        return self.model.evaluate(self.x_dev, self.y_dev)
 
     def predict(self, testDF):
         x_test = sequence.pad_sequences(np.array(testDF['tweet_ids']), maxlen=MAX_LEN)
         predictions = self.model.predict(x_test)
-        print(predictions[:5])
-        print(testDF['anger'][:5])
+        testDF[self.emotion + '_pred'] = predictions
+        tp = 0
+        fp = 0
+        tn = 0
+        fn = 0
+
+        for i, pred in enumerate(testDF[self.emotion + '_pred']):
+            if pred >= 0.5:
+                if testDF[self.emotion][i] == 1:
+                    tp += 1
+                else:
+                    fp += 1
+            if pred <= 0.5:
+                if testDF[self.emotion][i] == 1:
+                    fn += 1
+                else:
+                    tn += 1
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+        f1 = (precision * recall) / (precision + recall)
+
+        print("F1: {}\nPrecision: {}\nRecall: {}".format(f1, precision, recall))
 
 
 if __name__ == "__main__":
@@ -113,12 +143,23 @@ if __name__ == "__main__":
 
     trainDF, devDF = read_data(train_file, dev_file)
 
-    lstm_model = Sequential()
-    lstm_model.add(Embedding(VOCAB_SIZE, EMBEDDING_SIZE))
-    lstm_model.add(Bidirectional(LSTM(HIDDEN_SIZE)))
-    lstm_model.add(Dense(2, activation='tanh'))
-    lstm_model.add(Dense(1, activation='sigmoid'))
+    # lstm_model = Sequential()
+    # lstm_model.add(Embedding(VOCAB_SIZE, EMBEDDING_SIZE))
+    # lstm_model.add(Bidirectional(LSTM(HIDDEN_SIZE)))
+    # lstm_model.add(Dense(2, activation='tanh'))
+    # lstm_model.add(Dense(1, activation='sigmoid'))
 
-    angerNN = emotionNN(trainDF, devDF, 'anger', lstm_model)
+    # angerNN = emotionNN(trainDF, devDF, 'anger', lstm_model)
+
+    cnn_model = Sequential()
+    cnn_model.add(Embedding(VOCAB_SIZE, EMBEDDING_SIZE))
+    cnn_model.add(Conv1D(2 * HIDDEN_SIZE, kernel_size=3, activation='tanh', strides=1, padding='valid'))
+    cnn_model.add(GlobalMaxPooling1D())
+    cnn_model.add(Dense(HIDDEN_SIZE, activation='tanh'))
+    cnn_model.add(Dense(1, activation='sigmoid'))
+
+    angerNN = emotionNN(trainDF, devDF, 'anger', cnn_model)
+
     score, acc = angerNN.run()
     print("\nScore: {}, Accuracy: {}".format(score, acc))
+    angerNN.predict(devDF)
